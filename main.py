f1fb1f99897e2ddfc3cf473f0c65cd7eb1f933d3
philter1980 <62590523+philter1980@users.noreply.github.com>
2020-03-24 15:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os # Import the os library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 14 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(40, GPIO.OUT, initial=GPIO.HIGH) # Set pin 40 to be an OUTPUT pin for SELECTION
 
 index = 1
 
 
-while true
+while True:
 if GPIO.input(10) == GPIO.HIGH:
         print("MODE was pushed!")
                 index = -index
@@ -20,7 +20,7 @@ if GPIO.input(10) == GPIO.HIGH:
         if index == 1 :
                 GPIO.output(40, True)
                 
-if GPIO.input(14) == GPIO.HIGH:
+if GPIO.input(8) == GPIO.HIGH:
         print("SELECTION was pushed!")
         if index == -1 :
                 print ("SILENT MODE")
